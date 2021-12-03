fc8f8c9ffa7eb536a129c295e7e14430dcd1278c
wjdshs <95048056+wjdshs@users.noreply.github.com>
2021-12-03 13:30:16 +0900

    Add files via upload

--- 2146.cpp
+++ 2146.cpp
@@ -1,191 +1,96 @@
-#include <iostream>
-#include <queue>
-#include <algorithm>
-#include <cstring>
+#include<iostream>
+#include<queue>
+#include<deque>
+#include<vector>
+#include<algorithm>
+#include<string.h>
 using namespace std;
-const int INF = 987654321;
-const int MAX = 100;
-typedef struct
-{
-        int y, x;
-
-}Dir;
-
- 
-
-Dir moveDir[4] = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
-
- 
-
-int N;
-
-int graph[MAX][MAX];
-
-bool visited[MAX][MAX];
-
- 
-
-void DFS(int y, int x, int cnt)
-
-{
-
-        visited[y][x] = true;
-
-        graph[y][x] = cnt;
-
- 
-
-        for (int i = 0; i < 4; i++)
-
-        {
-
-                 int nextY = y + moveDir[i].y;
-
-                 int nextX = x + moveDir[i].x;
-
- 
-
-                 if (0 <= nextY && nextY < N && 0 <= nextX && nextX < N)
-
-                         if (graph[nextY][nextX] && !visited[nextY][nextX])
-
-                                 DFS(nextY, nextX, cnt);
-
+int N,m = 9999999;
+int maze[101][101];
+int visited[101][101];
+int number[101][101];
+int dy[4] = {-1,1,0,0};
+int dx[4] = {0,0,-1,1};
+int bfs(int cnt) {
+    queue < pair < int,int > > q;
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=N; j++) {
+            if(maze[i][j] == 1 && number[i][j] == cnt) {
+                q.push(make_pair(i,j));
+                visited[i][j] = 1;
+            }
         }
-
+    }
+    int step = 0;
+    while(!q.empty()) {
+        int qsize = q.size();
+        for(int i=0; i<qsize; i++) {
+            int y = q.front().first;
+            int x = q.front().second;
+            q.pop();
+            for(int i=0; i<4; i++) {
+                int newy = y + dy[i];
+                int newx = x + dx[i];
+                if(newy > N || newy <= 0 || newx > N || newx <= 0)
+                    continue;
+                if(visited[newy][newx])
+                    continue;
+                if(maze[newy][newx] && number[newy][newx] != cnt)
+                    return step;
+                if(maze[newy][newx] == 0) {
+                    visited[newy][newx] = 1;
+                    q.push(make_pair(newy,newx));
+                }
+            }
+        }
+        step++;
+    }
 }
-
  
-
-int BFS(int cnt)
-
-{
-
-        queue<pair<int, int>> q;
-
-        for(int i=0; i<N; i++)
-
-                 for(int j=0; j<N; j++)
-
-                         if (graph[i][j] == cnt)
-
-                         {
-
-                                 visited[i][j] = true;
-
-                                 q.push(make_pair(i, j));
-
-                         }
-
- 
-
-        int result = 0;
-
-        while (!q.empty())
-
-        {
-
-                 int curSize = q.size();
-                 for (int i = 0; i < curSize; i++)
-
-                 {
-
-                         int y = q.front().first;
-
-                         int x = q.front().second;
-
-                         q.pop();
-
- 
-
-                         for (int i = 0; i < 4; i++)
-
-                         {
-
-                                 int nextY = y + moveDir[i].y;
-
-                                 int nextX = x + moveDir[i].x;
-
-                                 if (0 <= nextY && nextY < N && 0 <= nextX && nextX < N)
-
-                                 {
-
-
-                                          if (graph[nextY][nextX] && graph[nextY][nextX] != cnt)
-
-                                                  return result;
-
-
-                                          else if (!graph[nextY][nextX] && !visited[nextY][nextX])
-
-                                          {
-
-                                                  visited[nextY][nextX] = true;
-
-                                                  q.push(make_pair(nextY, nextX));
-
-                                          }
-
-                                 }
-
-                         }
-
-                 }
-
-                 result++;
-
+void seperate(int y, int x, int cnt) {
+    queue < pair < int,int > > q;
+    visited[y][x] = 1;
+    number[y][x] = cnt;
+    q.push(make_pair(y,x));
+    while(!q.empty()) {
+        int ny = q.front().first;
+        int nx = q.front().second;
+        q.pop();
+        for(int i=0; i<4; i++) {
+            int newy = ny + dy[i];
+            int newx = nx + dx[i];
+            if(newy > N || newy <= 0 || newx > N || newx <= 0)
+                continue;
+            if(visited[newy][newx])
+                continue;
+            if(maze[newy][newx] == 1) {
+                visited[newy][newx] = 1;
+                q.push(make_pair(newy,newx));
+                number[newy][newx] = cnt;
+            }
         }
-
+    }
 }
-
  
-
-int main(void)
-
-{
-
-        cin >> N;
-
- 
-
-        for (int i = 0; i < N; i++)
-
-                 for (int j = 0; j < N; j++)
-
-                         cin >> graph[i][j];
-
- 
-
-        int cnt = 1;
-
-        //DFS를 통해 섬 표시
-
-        for (int i = 0; i < N; i++)
-
-                 for (int j = 0; j < N; j++)
-
-                         if (graph[i][j] && !visited[i][j])
-
-                                 DFS(i, j, cnt++);
-
- 
-
-        int result = INF;
-
-        for (int i = 1; i < cnt; i++)
-
-        {
-
-                 memset(visited, false, sizeof(visited));
-
-                 result = min(result, BFS(i));
-
+int main() {
+    int cnt = 0;
+    scanf("%d",&N);
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=N; j++) {
+            scanf("%1d",&maze[i][j]);
         }
-
- 
-
-        cout << result << endl;
-
-        return 0;
-
+    }
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=N; j++) {
+            if(visited[i][j] == 0 && maze[i][j] == 1) {
+                cnt++;
+                seperate(i,j,cnt);
+            }
+        }
+    }
+    for(int i=1; i<=cnt; i++) {
+        memset(visited,0,sizeof(visited));
+        m = min(m,bfs(i));
+    }
+    printf("%d\n",m);
 }
